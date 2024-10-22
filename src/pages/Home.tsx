66bd35f00e46d1cd13ab2e68c030334f126bd9be
angadthapa <119108341+Angad-Thapa26@users.noreply.github.com>
2024-10-22 09:11:28 +0545

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -123,14 +123,14 @@ const Home: React.FC<HomeProps> = ({ setCurrentPage }) => {
             </h2>
             <div className="flex items-center">
               <img
-                src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
+                src="https://shopratnaonline.com/wp-content/uploads/2023/10/Usle-Diyeko-Umer.webp"
                 alt="Book of the Week"
                 className="w-32 h-48 object-cover rounded-lg mr-4"
               />
               <div className="bow-12">
-                <h3 className="text-xl font-semibold mb-2">The Great Gatsby</h3>
-                <p className="text-gray-200 mb-2">by F. Scott Fitzgerald</p>
-                <p className="text-yellow-300 font-bold mb-2">20% OFF</p>
+                <h3 className="text-xl font-semibold mb-2">उसले दिएको उमेर</h3>
+                <p className="text-gray-200 mb-2">by बुद्धिसागर</p>
+                <p className="text-yellow-300 font-bold mb-2">2</p>
                 <button className="bg-yellow-400 text-blue-900 px-4 py-2 rounded-full hover:bg-yellow-300 transition duration-300 font-semibold">
                   About the book
                 </button>
@@ -166,10 +166,10 @@ const Home: React.FC<HomeProps> = ({ setCurrentPage }) => {
             </h2>
             <div className="bg-white bg-opacity-30 rounded-lg p-4 shadow-md">
               <h3 className="text-2xl font-semibold text-yellow-300 mb-2">
-                Back to School Sale!
+                Brightness of TIHAR Sale!
               </h3>
               <p className="text-gray-100">
-                Get 15% off on all school supplies until the end of the month.
+                Get exciting discount and gift hampers to lucky consumers this Tihar from us.
                 Don't miss out!
               </p>
               <button className="mt-4 bg-yellow-400 text-blue-900 px-6 py-2 rounded-full hover:bg-yellow-300 transition duration-300 font-semibold"></button>
