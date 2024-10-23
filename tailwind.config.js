64f804a40e32b78769bfd5d586b83fdbadc2878f
angadthapa <119108341+Angad-Thapa26@users.noreply.github.com>
2024-10-23 17:56:01 +0545

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mukta: ['Mukta', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
